e74227d3aacd626893219b7a40161781a6a78fd2
Lomas Persad <Lomaspersad@gmail.com>
2022-04-03 19:33:01 -0500

    test

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from bs4 import BeautifulSoup
 import requests
 import pickle
 
-#testing!!!
+#testing!!!!
 
 def get_data(selected_Url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
