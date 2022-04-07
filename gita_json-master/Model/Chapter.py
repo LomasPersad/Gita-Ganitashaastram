252eb197fd446d0a2c3ec8866d1b43bd1ba97d95
Lomas Persad <Lomaspersad@gmail.com>
2022-04-06 22:11:41 -0500

    BG data updated

---
+++ gita_json-master/Model/Chapter.py
@@ -0,0 +1,10 @@
+import json
+
+
+class Chapter:
+    def __init__(self):
+        self.chapter_number = ''
+        self.chapter_summary = ''
+        self.name = ''
+        self.name_meaning = ''
+        self.verses_count = ''
---
+++ gita_json-master/Model/Verse.py
@@ -0,0 +1,7 @@
+class Verse:
+    def __init__(self):
+        self.meaning = ''
+        self.text = ''
+        self.verse_number = ''
+        self.word_meanings = ''
+        self.transliteration = ''
