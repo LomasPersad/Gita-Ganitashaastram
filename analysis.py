bd04390aa1a659b2e6a24e6ee7a25e39293c4faa
Lomas Persad <Lomaspersad@gmail.com>
2022-04-09 22:12:26 -0500

    processing raw data

--- analysis.py
+++ analysis.py
@@ -2,9 +2,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from wordcloud import WordCloud,STOPWORDS
-import os
+#import seaborn as sns
+#from wordcloud import WordCloud,STOPWORDS
+#import os
+import json
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
@@ -13,6 +14,20 @@ import os
 # https://en.wikipedia.org/wiki/Sanskrit_nominals#Pronouns_and_determiners
 
 
-print(os.listdir("../input"))
+# Opening JSON file
+f = open('LP_dataset_english.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+# Closing file
+f.close()
+
+# Iterating through the json
+# list
+for i in data['chapters']:
+    print(data['chapters'][i]['name'])
+
+
 
 # Any results you write to the current directory are saved as output.
